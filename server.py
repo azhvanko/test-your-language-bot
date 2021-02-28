3b99a9117734cddb72f954f5cbebe00722da6c48
Artsiom Zhvanko <azhvanko@outlook.com>
2021-02-28 17:30:27 +0300

    Upd server.py()

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 import os.path
-from typing import NoReturn, Tuple, Union
+from typing import NoReturn, Sequence, Tuple, Union
 
 from aiogram import Bot, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -10,8 +10,14 @@ from aiogram.types import ContentType
 from aiogram.utils import executor
 
 from core.config import BASE_DIR, TOKEN
+from core.db import close_connection, create_connection
 from core.dispatcher import SessionsDispatcher
-from core.types import Answer
+from core.init_db import check_db_exists
+from core.handlers import (
+    language_test_creator_session_handler,
+    user_session_handler
+)
+from core.types import Answer, CloseSession
 
 
 logging.basicConfig(
@@ -24,7 +30,12 @@ logging.basicConfig(
 
 
 dp = SessionsDispatcher()
+dp.register_handlers(
+    language_test_creator_session_handler,
+    user_session_handler,
+)
 loop = asyncio.get_event_loop()
+loop.create_task(dp.close_old_sessions())
 bot = Bot(token=TOKEN)
 dispatcher = Dispatcher(bot, storage=MemoryStorage(), loop=loop)
 
@@ -45,15 +56,34 @@ async def process_document(message: types.Message) -> None:
 
 
 async def _process_answers(user_id: int, answers: Union[Answer, Tuple]) -> None:
-    pass
+    if isinstance(answers, Answer):
+        await _send_answer(user_id, answers)
+    elif isinstance(answers, Sequence):
+        for answer in answers:
+            if isinstance(answer, Answer):
+                await _send_answer(user_id, answer)
+            elif isinstance(answer, CloseSession):
+                dp.close_session(user_id)
 
 
-async def _send_answer() -> None:
-    pass
+async def _send_answer(user_id: int, answer: Answer) -> None:
+    await bot.send_message(
+        chat_id=user_id,
+        text=answer.text,
+        reply_markup=answer.keyboard
+    )
+
+
+async def on_shutdown(_):
+    close_connection()
 
 
 def main() -> NoReturn:
-    executor.start_polling(dispatcher, skip_updates=True, timeout=60)
+    create_connection('language_bot_db.db')
+    check_db_exists()
+    executor.start_polling(
+        dispatcher, skip_updates=True, timeout=60, on_shutdown=on_shutdown
+    )
 
 
 if __name__ == '__main__':
